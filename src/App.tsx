b5fef4bc2bddb921d76aa315ac5bf7d51043a9c4
Iain Maitland <iain@iainmaitland.com>
2019-09-09 16:18:35 -0400

    test linters

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ const App: React.FC = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Just a test.{non}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,6 +22,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
